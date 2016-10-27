1d032d5638069c5317e05a85cad84ce11a8d85ef
ManuelMonteiro24 <tic-manuel13b@hotmail.com>
2016-10-27 20:49:28 +0100

    menu + criar mail

--- client_manel.go
+++ client_manel.go
@@ -6,7 +6,7 @@ import "os"
 func main() {
     fmt.Printf("Hello welcome to the Distributed email\n")
         for {
-            fmt.Printf("Menu\n 1. New email\n 2. Check your inbox\n 3. Exit\n Enter Number:\n")
+          fmt.Printf("Menu\n 1.New email 2.Check your inbox 3. Exit\n Enter Number:\n")
             menu()
         }
 }
@@ -20,14 +20,31 @@ func menu(){
   }
   switch input {
   case 1:
-      fmt.Println("Chamar funcao criar mail")
+      mail_information()
   case 2:
-        fmt.Println("Chamar funcao check inbox")
+        fmt.Println("Chamar funcao check inbox\n")
   case 3:
-      fmt.Printf("Hello welcome to the Distributed email\n")
+      fmt.Println("Goodbye thank you for choosing us\n")
       os.Exit(0)
   default:
       fmt.Println("Invalid Input\n")
   }
 
+}
+
+func mail_information(){
+  fmt.Println("Email information\n")
+  var i = 1
+  for i > 0{
+    fmt.Println("From: \n")
+    var input string
+    n, err := fmt.Scanln(&input)
+    if n < 1 || n > 2 || err != nil {
+      fmt.Println("Invalid input\n")
+      return
+    } else {
+      i = 0
+      fmt.Println(input)
+    }
+  }
 }
