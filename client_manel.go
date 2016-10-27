2cc8d5f30ac3e06cd048d3c590fc08394d3f0d8b
ManuelMonteiro24 <tic-manuel13b@hotmail.com>
2016-10-27 21:30:21 +0100

    Menu changes, Recebe o destinatario
    algumas duvidas em relação ao retorno das funções, e sobre slices tbm

--- client_manel.go
+++ client_manel.go
@@ -3,14 +3,18 @@ package main
 import "fmt"
 import "os"
 
+
 func main() {
     fmt.Printf("Hello welcome to the Distributed email\n")
+
+    //Authentication????
+
         for {
           fmt.Printf("Menu\n 1.New email 2.Check your inbox 3. Exit\n Enter Number:\n")
             menu()
         }
 }
-
+//Menu inicial ?? adaptar ao metodo de autenticação do utilizador
 func menu(){
   var input int
   n, err := fmt.Scanln(&input)
@@ -20,9 +24,9 @@ func menu(){
   }
   switch input {
   case 1:
-      mail_information()
+      fmt.Println(Dest_information()) //Como se guarda o valor do que retorna da funcao?
   case 2:
-        fmt.Println("Chamar funcao check inbox\n")
+      fmt.Println("Chamar funcao check inbox\n")
   case 3:
       fmt.Println("Goodbye thank you for choosing us\n")
       os.Exit(0)
@@ -32,19 +36,19 @@ func menu(){
 
 }
 
-func mail_information(){
+func Dest_information() (dest string){
   fmt.Println("Email information\n")
-  var i = 1
-  for i > 0{
-    fmt.Println("From: \n")
+  for {
+    fmt.Println("To: \n")
     var input string
     n, err := fmt.Scanln(&input)
     if n < 1 || n > 2 || err != nil {
       fmt.Println("Invalid input\n")
-      return
     } else {
-      i = 0
+      return input
       fmt.Println(input)
+      }
     }
   }
-}
+//Meter aqui funcao do amado que recebe o texto que o utilizador quer enviar
+//e introduz num ficheiro para enviar???
