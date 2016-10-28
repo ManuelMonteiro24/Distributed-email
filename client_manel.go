33f44129f063089b78ea150968f3152f74237ac2
amadilsons <joao.amado.95@gmail.com>
2016-10-28 09:14:40 +0800

    client_manel minor edit

--- client_manel.go
+++ client_manel.go
@@ -10,7 +10,7 @@ func main() {
     //Authentication????
 
         for {
-          fmt.Printf("Menu\n 1.New email 2.Check your inbox 3. Exit\n Enter Number:\n")
+          fmt.Printf("Menu\n 1.New email 2.Check your inbox 3. Exit\n")
             menu()
         }
 }
@@ -22,28 +22,29 @@ func menu(){
     fmt.Println("Invalid input\n")
     return
   }
+
   switch input {
-  case 1:
-      fmt.Println(Dest_information()) //Como se guarda o valor do que retorna da funcao?
-  case 2:
-      fmt.Println("Chamar funcao check inbox\n")
-  case 3:
-      fmt.Println("Goodbye thank you for choosing us\n")
-      os.Exit(0)
-  default:
-      fmt.Println("Invalid Input\n")
+        case 1: save_info := Dest_information()
+                fmt.Println(save_info) 
+        case 2:
+                fmt.Println("Chamar funcao check inbox")
+        case 3:
+                fmt.Println("Goodbye thank you for choosing us")
+                os.Exit(0)
+        default:
+                fmt.Println("Invalid Input")
   }
 
 }
 
 func Dest_information() (dest string){
-  fmt.Println("Email information\n")
+  fmt.Println("Email information")
   for {
-    fmt.Println("To: \n")
+    fmt.Println("To: ")
     var input string
     n, err := fmt.Scanln(&input)
     if n < 1 || n > 2 || err != nil {
-      fmt.Println("Invalid input\n")
+      fmt.Println("Invalid input")
     } else {
       return input
       fmt.Println(input)
