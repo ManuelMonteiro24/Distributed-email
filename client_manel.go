45a1e2d69749c28d3a5752675f479f19ba2c9dbb
ManuelMonteiro24 <tic-manuel13b@hotmail.com>
2016-12-20 15:40:49 +0000

    recebe destinatario e ficheiro

--- client_manel.go
+++ client_manel.go
@@ -17,18 +17,18 @@ func main() {
     fmt.Println("User: ", user_name)
         for {
           fmt.Printf("Main Menu\n 1.New email 2.Check your inbox 3. Exit\n")
-            menu()
+            menu(user_name)
         }
 }
 
 func auth_user()(string){
   var user_name = data_input("Insert Username: ")
   // detect if key file associated to user exists
-	 _, err := os.Stat(user_name)
+	 _, err := os.Stat(user_name + "_PrivateKey")
 
 	  if os.IsNotExist(err) {
-    //create file
-		var user_file, err = os.Create(user_name)
+    //create user key file
+		var user_file, err = os.Create(user_name + "_PrivateKey")
 		checkError(err)
     defer user_file.Close()
 
@@ -45,13 +45,31 @@ func auth_user()(string){
             Bytes: x509.MarshalPKCS1PrivateKey(userKey),
         },
     )
-    ioutil.WriteFile(user_name, pemdata, 0644)
+    ioutil.WriteFile(user_name + "_PrivateKey", pemdata, 0644)
+
+    //create user PublicKey contact file
+    PublicKey, err := x509.MarshalPKIXPublicKey(&userKey.PublicKey)
+    checkError(err)
+
+    pemdata = pem.EncodeToMemory(
+        &pem.Block{
+            Type: "RSA PUBLIC KEY",
+            Bytes: PublicKey,
+        },
+    )
+    var contact_name string = user_name + "_PublicKey"
+    contact_file, err := os.Create(contact_name)
+		checkError(err)
+    defer contact_file.Close()
+
+    ioutil.WriteFile(contact_name, pemdata, 0644)
+
 		fmt.Printf("User keys generated!\n")
 
   }else{
-    //
+    //load user key from file
     fmt.Printf("User login successful!\n")
-    file_data, err := ioutil.ReadFile(user_name)
+    file_data, err := ioutil.ReadFile(user_name + "_PrivateKey")
     checkError(err)
     pemdata, _ := pem.Decode(file_data)
     userKey, err := x509.ParsePKCS1PrivateKey(pemdata.Bytes)
@@ -62,7 +80,7 @@ func auth_user()(string){
   return user_name
 }
 
-func menu(){
+func menu(user_name string){
   var input int
   n, err := fmt.Scanln(&input)
   if n < 1 || n > 2 || err != nil {
@@ -72,6 +90,10 @@ func menu(){
   switch input {
         case 1:
                 fmt.Println("New email option chosen\n")
+                dest_name := rcv_dest()
+                file_name := file_to_send()
+                fmt.Println("Sender:", user_name, "\nDest :", dest_name, "\nFile: ", file_name,"\n")
+
         case 2:
                 fmt.Println("Check inbox option chosen\n")
         case 3:
@@ -82,18 +104,34 @@ func menu(){
   }
 }
 
-func rcv_dest() {
-    //recieve contact
+func rcv_dest()(string){
+    //recieve name of contact
     var recp_name = data_input("Insert Recipient: ")
 
-    //check if contact exists
-    var _, err = os.Stat(/contacts/recp_name)
+    //Check if contact exists
+    _, err := os.Stat(recp_name + "_PublicKey")
   	if os.IsNotExist(err) {
       fmt.Println(err.Error())
   		os.Exit(3)
     } else{
-      
+      fmt.Println("Contact found!\n")
     }
+    return recp_name
+}
+
+func file_to_send()(string){
+    //recieve name of file
+    var file_name = data_input("Insert name of file to send: ")
+
+    //Check if file exists
+    _, err := os.Stat(file_name)
+  	if os.IsNotExist(err) {
+      fmt.Println(err.Error())
+  		os.Exit(3)
+    } else{
+      fmt.Println("File found!\n")
+    }
+    return file_name
 }
 
 func data_input(msg string) (string){
