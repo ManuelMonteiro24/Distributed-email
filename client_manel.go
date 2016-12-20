6178efef23d55cfc8a4820e53e01bdbd2b93eaf1
ManuelMonteiro24 <tic-manuel13b@hotmail.com>
2016-12-20 03:10:04 +0000

    gera par de chaves para utilizador
    passar a PrivateKey para fora da função????

--- client_manel.go
+++ client_manel.go
@@ -1,20 +1,67 @@
 package main
 
-import "fmt"
-import "os"
+import (
+  "crypto/rand"
+  "crypto/rsa"
+  "fmt"
+  "os"
+  "encoding/pem"
+  "io/ioutil"
+  "crypto/x509"
+)
 
-/*
+
 func main() {
     fmt.Printf("Hello welcome to the Distributed email\n")
-
-    //Authentication????
-
+     user_name := auth_user()
+    fmt.Println("User: ", user_name)
         for {
-          fmt.Printf("Menu\n 1.New email 2.Check your inbox 3. Exit\n")
+          fmt.Printf("Main Menu\n 1.New email 2.Check your inbox 3. Exit\n")
             menu()
         }
 }
-//Menu inicial ?? adaptar ao metodo de autenticação do utilizador*/
+
+func auth_user()(string){
+  var user_name = data_input("Insert Username: ")
+  // detect if key file associated to user exists
+	 _, err := os.Stat(user_name)
+
+	  if os.IsNotExist(err) {
+    //create file
+		var user_file, err = os.Create(user_name)
+		checkError(err)
+    defer user_file.Close()
+
+    // generate user key
+    userKey, err := rsa.GenerateKey(rand.Reader, 2048)
+    checkError(err)
+
+    fmt.Println("User Key : ", userKey)
+
+    //encode key to file
+    pemdata := pem.EncodeToMemory(
+        &pem.Block{
+            Type: "RSA PRIVATE KEY",
+            Bytes: x509.MarshalPKCS1PrivateKey(userKey),
+        },
+    )
+    ioutil.WriteFile(user_name, pemdata, 0644)
+		fmt.Printf("User keys generated!\n")
+
+  }else{
+    //
+    fmt.Printf("User login successful!\n")
+    file_data, err := ioutil.ReadFile(user_name)
+    checkError(err)
+    pemdata, _ := pem.Decode(file_data)
+    userKey, err := x509.ParsePKCS1PrivateKey(pemdata.Bytes)
+    checkError(err)
+
+    fmt.Println("User Key : ", userKey)
+  }
+  return user_name
+}
+
 func menu(){
   var input int
   n, err := fmt.Scanln(&input)
@@ -22,19 +69,44 @@ func menu(){
     fmt.Println("Invalid input\n")
     return
   }
-
   switch input {
-        case 1: save_info := Dest_information()
-                fmt.Println(save_info)
+        case 1:
+                fmt.Println("New email option chosen\n")
         case 2:
-                fmt.Println("Chamar funcao check inbox")
+                fmt.Println("Check inbox option chosen\n")
         case 3:
-                fmt.Println("Goodbye thank you for choosing us")
+                fmt.Println("Goodbye thank you for choosing us\n")
                 os.Exit(0)
         default:
-                fmt.Println("Invalid Input")
+                fmt.Println("Invalid Input\n")
   }
+}
 
+func rcv_dest() {
+    //recieve contact
+    var recp_name = data_input("Insert Recipient: ")
+
+    //check if contact exists
+    var _, err = os.Stat(/contacts/recp_name)
+  	if os.IsNotExist(err) {
+      fmt.Println(err.Error())
+  		os.Exit(3)
+    } else{
+      
+    }
+}
+
+func data_input(msg string) (string){
+  for{
+    var data string
+      fmt.Println(msg)
+    n, err := fmt.Scanln(&data)
+      if n < 1 || n > 2 || err != nil {
+        fmt.Println("Invalid input\n")
+      }else{
+        return data
+      }
+  }
 }
 
 func Dest_information() (dest string){
@@ -50,6 +122,11 @@ func Dest_information() (dest string){
       fmt.Println(input)
       }
     }
-  }
-//Meter aqui funcao do amado que recebe o texto que o utilizador quer enviar
-//e introduz num ficheiro para enviar???
+}
+
+func checkError(err error) {
+  	if err != nil {
+  		fmt.Println(err.Error())
+  		os.Exit(0)
+  	}
+}
