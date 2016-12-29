aabd6bee9c71b45c5ea39a4dd65c30a281ff56a4
colobas <guilherme.pg.pires@gmail.com>
2016-12-29 18:48:30 +0000

    refact

---
+++ kademlia/pkey.go
@@ -0,0 +1,22 @@
+package kademlia
+
+import (
+    "bytes"
+    "golang.org/x/crypto/openpgp"
+    "io/ioutil"
+)
+
+func getEntityFromFile(path string) (privkey *openpgp.Entity, err error) {
+    b, err := ioutil.ReadFile(path) 
+    if err != nil {
+        return nil, err
+    }
+
+    privkeyList,err := openpgp.ReadArmoredKeyRing(bytes.NewReader(b))
+    if err != nil {
+        return nil, err
+    }
+    privkey = privkeyList[0]
+
+    return privkey, nil
+}
